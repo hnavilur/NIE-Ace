42efef46a7b677c10fcc18299dbc0b26ac757519
navilur <hariprasadnavilurn@gmail.com>
2014-02-23 15:47:36 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+NIE-Ace</title>
+</head>
+<body>
+<h2>Welcome to Nie-Ace<h2>
+Username:<input type="text" name="username" id="login_box"/>
+Password:<input type="password" name="password" id="login_box"/>
+Not yet registered? <a href="register.php" id="signup_link">Register Now</a>
+</body>
+</html>
